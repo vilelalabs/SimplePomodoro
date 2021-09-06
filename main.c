64210b1702706309fc674715ac4b3c7d890d98f5
Henrique Vilela <39452510+vilelalabs@users.noreply.github.com>
2021-09-06 12:18:51 -0300

    Better comments and code view

--- main.c
+++ main.c
@@ -2,29 +2,29 @@
 #include "config.h"
 #include "pins.h"
 
-
-bool stop_beep = false;
+bool stop_beep = false;         
 
 void setup(void);
 
-
+//catch button at any time TMR0 interrupt
 void __interrupt() btn(void){
     if(INTCONbits.T0IF){
         INTCONbits.T0IF = 0;
         if(!BUTTON){
             stop_beep = true;
-            __delay_ms(1000);
+            __delay_ms(700);
         }
     }
 }
 
+//get time in minutes
 void tempo (int t_min){
     for(unsigned int i = 0; i < (t_min*60); i++)
         __delay_ms(1000);  
 }
 
 void focusBeep(void){
-    while(!stop_beep){ // enquanto o botão nao for pressionado não para o alarme
+    while(!stop_beep){  //beep while button is not pressed
         for(char i = 0; i<5;i++){
             BUZZER = 1;
             LED_R = 0;
@@ -40,7 +40,7 @@ void focusBeep(void){
 }
 
 void restTime(void){
-    while(!stop_beep){ // enquanto o botão nao for pressionado não para o alarme
+    while(!stop_beep){ //beep while button is not pressed
         BUZZER = 1;
         LED_B = 0;
         __delay_ms(100);
@@ -53,59 +53,57 @@ void restTime(void){
 
 void beep_start(void){
     for(char i = 0; i<3;i++){
-            BUZZER = 1;
-            LED_G = 0;
-            __delay_ms(200);
-            BUZZER = 0;
-            LED_G = 1;
-            __delay_ms(200);
-        }
-    
+        BUZZER = 1;
+        LED_G = 0;
+        __delay_ms(200);
+        BUZZER = 0;
+        LED_G = 1;
+        __delay_ms(200);
+    }    
 }
 
 void main(void) {
     setup();
     
-    loop(){
-        
-        //ciclos de 25 / 5 pomodoro
-        tempo(25);//25
+    loop(){    
+        // 25/5 pomodoro cycle
+        tempo(25);              //25 minutes
         focusBeep();
-        tempo(5);//5
-        restTime();
-        
-    }
-        
+        tempo(5);               //05 minutes
+        restTime();   
+    }    
     return;
 }
 
 void setup(void){
     
-    TRISBbits.TRISB1 = 0 ; // RB1 = saída
-    TRISBbits.TRISB2 = 0 ; // RB2  = saída
-    TRISBbits.TRISB3 = 0 ; // RB3 = saída
-    TRISBbits.TRISB4 = 1 ; // RB4 = entrada
-    TRISBbits.TRISB5 = 0 ; // RB5 = entrada
+    TRISBbits.TRISB1 = 0 ;      // RB1 = saÃ­da   / out
+    TRISBbits.TRISB2 = 0 ;      // RB2  = saÃ­da  / out
+    TRISBbits.TRISB3 = 0 ;      // RB3 = saÃ­da   / out
+    TRISBbits.TRISB4 = 1 ;      // RB4 = entrada / in
+    TRISBbits.TRISB5 = 0 ;      // RB5 = entrada / out
     
+    // interrupt bits config
     INTCONbits.GIE = 1;
     INTCONbits.T0IE = 1;
     INTCONbits.T0IF = 0;
     
+    //timer config bits
     OPTION_REGbits.T0CS = 0;
     OPTION_REGbits.PSA = 0;
     OPTION_REGbits.PS = 0b111;
     TMR0 = 0;
     
-    //inicializa dispositivos
-    
-    //inicializa LEDs apagados (lógica inversa)
+    // start all RGB colors off (reverse logic)
+    // inicializa as cores RGB apagadas (lÃ³gica inversa)
     LED_R = 1; 
     LED_G = 1; 
     LED_B = 1; 
     
-    BUZZER = 0; //inicializa BUZZER desligado
+    // starts the buzzer off
+    //inicializa BUZZER desligado
+    BUZZER = 0; 
     
     beep_start();
-
     return;
 }
