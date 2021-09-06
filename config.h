ec65705755db20188806eb73b91dfb4ebb1cc6a8
Henrique Vilela <39452510+vilelalabs@users.noreply.github.com>
2021-09-06 12:09:13 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+#ifndef CONFIG_H
+#define	CONFIG_H
 
 
 // PIC16F84A Configuration Bit Settings
@@ -11,17 +11,17 @@
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (Power-up Timer is disabled)
 #pragma config CP = OFF         // Code Protection bit (Code protection disabled)
 
-#include <xc.h>
-
-#define _XTAL_FREQ 4000000UL // Cristal
-
 //create boolean type for use in C
 #define  bool int
 #define  true 1
 #define  false 0
 
-//define função de looping infinito
+//define funÃ§Ã£o de looping infinito
 #define loop() while(1)
 
-#endif	/* XC_HEADER_TEMPLATE_H */
+#include <xc.h>                 // include for XC8 compiler
+
+#define _XTAL_FREQ 4000000UL    // Crystal frequency
+
+#endif	/* CONFIG_H */
 
